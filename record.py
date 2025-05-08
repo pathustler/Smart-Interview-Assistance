80f8e63a680d552e8d24057db5c9eb27d8448128
pathustler <patlowharry@gmail.com>
2025-05-08 22:45:04 +1000

    code to record audio and save as .wav

--- record.py
+++ record.py
@@ -4,7 +4,7 @@ from scipy.io.wavfile import write
 def record_audio(filename="interview.wav", duration=10, fs=16000):
     print("Recording...")
     audio = sd.rec(int(duration * fs), samplerate=fs, channels=1, dtype='int16')
-    sd.wait()  # Wait until recording is finished
+    sd.wait()  
     write(filename, fs, audio)
     print("Done recording.")
 
